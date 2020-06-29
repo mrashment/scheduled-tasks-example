997c07a01d4d86295c9d3c28c9ce9c5fb8eea937
Mason Ashment <masonashment@gmail.com>
2020-06-28 21:28:07 -0400

    Working toast

--- app/src/main/java/com/mrashment/scheduledtasksexample/DelayedMessageIntentService.kt
+++ app/src/main/java/com/mrashment/scheduledtasksexample/DelayedMessageIntentService.kt
@@ -2,14 +2,32 @@ package com.mrashment.scheduledtasksexample
 
 import android.app.IntentService
 import android.content.Intent
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 
+
 class DelayedMessageIntentService : IntentService("DelayedMessageIntentService") {
+
+    lateinit var handler: Handler
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        handler = Handler()
+        return super.onStartCommand(intent, flags, startId)
+    }
 
     override fun onHandleIntent(intent: Intent?) {
         Log.d("MyService","Started service $this")
         Thread.sleep(5000)
-        Toast.makeText(applicationContext,"Wow! You just ran a service!",Toast.LENGTH_LONG).show()
+        message()
+    }
+
+    fun message() {
+        handler.post(object: Runnable {
+            override fun run() {
+                Toast.makeText(applicationContext,"Wow! You just ran a service!",Toast.LENGTH_LONG).show()
+            }
+        })
+
     }
 }
